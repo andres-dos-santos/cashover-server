195f2c4e3b58d7c6a234a72dbbfa927360c072d4
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-11-20 16:55:05 -0300

    feat(models/get-companies.model): select doc, name & id to return

--- src/models/get-companies.model.ts
+++ src/models/get-companies.model.ts
@@ -1,5 +1,7 @@
 import { prisma } from '../db/prisma';
 
 export async function getCompanies() {
-  return await prisma.company.findMany();
+  return await prisma.company.findMany({
+    select: { doc: true, name: true, id: true },
+  });
 }
