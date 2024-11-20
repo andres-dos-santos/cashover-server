07515b836abe149e5dad0bc408b3a1265dca5f0c
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-11-19 21:12:08 -0300

    feat: add app.use

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import { companyRoutes } from './routes/company.routes';
+
 const app = express();
 app.use(express.json());
 
-app.get('/health', (_, res) => {
+app.use(companyRoutes);
+
+app.get('/health', (req, res) => {
   return res.status(200).json({ status: 'Ok!' });
 });
 
