28d3b7552f486548ab3023c06387df233d9d64f7
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-11-22 15:09:52 -0300

    test(group.controller.test): create test about create-group

--- src/controllers/group.controller.test.ts
+++ src/controllers/group.controller.test.ts
@@ -1,6 +1,8 @@
-import { expect, test } from 'bun:test';
+import { beforeAll, expect, test } from 'bun:test';
+import * as uuid from 'uuid';
 
 import { baseURL } from '../constants/base-url';
+import { prisma } from '../db/prisma';
 
 const group = {
   name: 'Group 1',
@@ -10,12 +12,27 @@ const group = {
 async function createGroup() {
   return await fetch(baseURL + 'group', {
     method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(group),
   });
 }
+
+beforeAll(async () => {
+  await prisma.group.deleteMany();
+});
 
 test('deve criar um novo grupo', async () => {
   const response = await createGroup();
 
   expect(response.status).toBe(201);
+
+  const data = await response.json();
+
+  expect(uuid.validate(data.id)).toBeTruthy();
+});
+
+test('deve falhar ao criar um novo grupo', async () => {
+  const response = await createGroup();
+
+  expect(response.status).toBe(400);
 });
--- src/models/get-group.model.ts
+++ src/models/get-group.model.ts
@@ -1,6 +1,8 @@
 import { prisma } from '../db/prisma';
 
 export async function getGroup(name: string) {
+  console.log('name', name);
+
   return await prisma.group.findUnique({
     where: { name },
   });
